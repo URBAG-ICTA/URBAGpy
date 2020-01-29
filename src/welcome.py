e3fbe9a3fee1ea870d5ea2dd4d9821d1c34c11b7
Marcelsj <marcel.serra@gmail.com>
2020-01-29 16:17:16 +0100

    added welcome

---
+++ src/welcome.py
@@ -0,0 +1,7 @@
+
+class Welcome:
+        
+    def welcome():
+        print("Welcome to URBAGpy")
+        print("You are ready to start learning Python")
+        print("Your first goal is to understand why this message has appeared in the screen.")
